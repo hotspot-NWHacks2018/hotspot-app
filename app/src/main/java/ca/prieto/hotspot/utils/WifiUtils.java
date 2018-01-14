77a31cbfbf8dc2d50b6491f7ec9d3a8f16c180e1
George Antonious <george@antonious.ca>
2018-01-13 22:41:12 -0800

    Add wifi-utils

---
+++ app/src/main/java/ca/prieto/hotspot/utils/WifiUtils.java
@@ -0,0 +1,34 @@
+package ca.prieto.hotspot.utils;
+
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+
+import io.reactivex.Completable;
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
+import static android.content.Context.WIFI_SERVICE;
+
+/**
+ * Created by George on 2018-01-13.
+ */
+
+public class WifiUtils {
+
+    public static Completable connectToWifi(Context context, String ssid, String password) {
+        WifiConfiguration wifiConfig = new WifiConfiguration();
+        wifiConfig.SSID = String.format("\"%s\"", ssid);
+        wifiConfig.preSharedKey = String.format("\"%s\"", password);
+
+        return Completable.fromAction(() -> {
+            WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(WIFI_SERVICE);
+            int netId = wifiManager.addNetwork(wifiConfig);
+            wifiManager.disconnect();
+            wifiManager.enableNetwork(netId, true);
+            wifiManager.reconnect();
+        })
+        .subscribeOn(Schedulers.io());
+    }
+
+}
